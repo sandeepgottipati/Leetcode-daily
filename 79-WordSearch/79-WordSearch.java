f0cbdf03123a6c7bfaad879e3696ad82334f6846
sandeep gottipati <51874917+sandeepgottipati@users.noreply.github.com>
2024-04-03 17:52:41 -0500

    Create file

---
+++ 79-WordSearch/79-WordSearch.java
@@ -0,0 +1,26 @@
+class Solution {
+    static boolean visited[][];
+    public boolean exist(char[][] board, String word) {
+         int row=board.length;
+         int column=board[0].length;
+         visited=new boolean[row][column];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                if(word.charAt(0)==board[i][j] && search(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+        
+    }
+   boolean search(char[][]board,String word,int i,int j,int index){
+        if(index==word.length()){
+            return true;
+        }
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || word.charAt(index)!=board[i][j] || visited[i][j]){
+            return false;
+        }
+        visited[i][j]=true;
+[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
