e16384b0fb9db42313ba5a64d0a68af91ca60f77
sandeep gottipati <51874917+sandeepgottipati@users.noreply.github.com>
2024-04-12 18:20:44 -0500

    Create file

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,27 @@
+            for(int i=0;i<height.length;i++){
+            int maxLeft=0;
+    int left[]=new int[height.length];
+/**
+
+}
+    }
+        return res;
+        }
+                if(maxLeft<height[i]){
+                    maxLeft=height[i];
+                }
+                left[i]=maxLeft;
+            }
+            }
+                right--;
+                rightMax=Math.max(rightMax,height[right]);
+ res+=Math.max(rightMax-height[right],0);
+            else{
+            }
+                left++;
+                leftMax=Math.max(leftMax,height[left]);
+                res+=Math.max(leftMax-height[left],0);
+            if(leftMax<rightMax){
+        while(left<=right){
+        int res=0;
+[0,1,0,2,1,0,1,3,2,1,2,1]
